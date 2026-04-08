2f2e9b9f750ea2b8f4552b9bf29e1c14f2ecd10e
Thedarklee <javiera.alarcon042003@gmail.com>
2026-04-08 18:16:12 -0400

    agregar pestañas

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -30,11 +30,11 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
 
           
            <Link href="/dashboard/vehiculos" className={`flex items-center gap-3 px-6 py-3 text-sm font-medium transition-all ${pathname === '/dashboard/vehiculos' ? 'text-violet-900 border-l-4 border-emerald-600 bg-slate-50' : 'text-slate-500 hover:text-violet-700 hover:bg-slate-50'}`}>
-            <span className="material-symbols-outlined">Flota de Vehículos</span> Conductores
+            <span className="material-symbols-outlined">Flota de Vehículos</span> Flota de Vehículos
           </Link>
 
-                     <Link href="/dashboard/accesos/nuevo" className={`flex items-center gap-3 px-6 py-3 text-sm font-medium transition-all ${pathname === '/dashboard/accesos/nuevo' ? 'text-violet-900 border-l-4 border-emerald-600 bg-slate-50' : 'text-slate-500 hover:text-violet-700 hover:bg-slate-50'}`}>
-            <span className="material-symbols-outlined">Nuevo Usuario</span> Conductores
+                     <Link href="/dashboard/usuarios/nuevo" className={`flex items-center gap-3 px-6 py-3 text-sm font-medium transition-all ${pathname === '/dashboard/usuarios/nuevo' ? 'text-violet-900 border-l-4 border-emerald-600 bg-slate-50' : 'text-slate-500 hover:text-violet-700 hover:bg-slate-50'}`}>
+            <span className="material-symbols-outlined">Nuevo Usuario</span> Usuarios
           </Link>
 
         </nav>
